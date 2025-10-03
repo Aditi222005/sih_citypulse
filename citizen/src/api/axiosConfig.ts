7d015b1a1846a38b4037708dc2714b2555bd199e
joshiAditi2005 <aditisjoshi2005@gmail.com>
2025-10-03 22:35:11 +0530

    frontend configured

--- citizen/src/api/axiosConfig.ts
+++ citizen/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://citypulse-backend-git-main-aditisjoshi2005-gmailcoms-projects.vercel.app/api", 
+  baseURL: "https://citypulse-backend-git-main-aditisjoshi2005-gmailcoms-projects.vercel.app", 
   withCredentials: true, // ✅ allow sending cookies/credentials
   headers: {
     "Content-Type": "application/json",
